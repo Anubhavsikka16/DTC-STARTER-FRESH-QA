a7a813c9711028f27c4a084cae8725365ee7dcfc
Anubhavsikka16 <anubhavsikka@gmail.com>
2026-08-14 00:52:50 +0530

    Add logs to debug

--- tests/smoke/test_login_admin.py
+++ tests/smoke/test_login_admin.py
@@ -5,6 +5,9 @@ class TestAdminLogin:
     @pytest.mark.smoke 
     def test_admin_login(self, login_service):
 
+        print("Admin email:", settings.admin_email)
+        print("Admin password configured:", bool(settings.admin_password))
+
         dashboard=login_service.login(
             settings.admin_email,
             settings.admin_password
