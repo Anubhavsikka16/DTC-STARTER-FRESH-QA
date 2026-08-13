efd0a69cd38d23c488dc112d5e52ff3ef5ce38a4
Anubhavsikka16 <anubhavsikka@gmail.com>
2026-08-14 01:34:24 +0530

    Adding logs

--- framework/config/settings.py
+++ framework/config/settings.py
@@ -2,35 +2,49 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+
+    # ==================================================
     # Application
+    # ==================================================
     base_url: str
     api_base_url: str
-    
 
+    # ==================================================
     # Admin
+    # ==================================================
     admin_email: str
     admin_password: str
 
+    # ==================================================
     # Browser
-    browser: str
-    headless: bool
-    slow_mo: int
-    timeout: int
+    # ==================================================
+    browser: str = "chromium"
+    headless: bool = True
+    slow_mo: int = 0
+    timeout: int = 30000
 
+    # ==================================================
     # Database
-    db_host: str
-    db_port: int
-    db_name: str
-    db_user: str
-    db_password: str
+    # ==================================================
+    db_host: str = "localhost"
+    db_port: int = 5432
+    db_name: str = "medusa-backend"
+    db_user: str = "postgres"
+    db_password: str = "postgres"
 
     model_config = SettingsConfigDict(
         env_file=".env",
-        case_sensitive=False
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
     )
 
 
-settings = Settings() #settings.base_url: easy access instead of adding Settings() every time
+settings = Settings()
+
 
 if __name__ == "__main__":
-    print(settings)#"Only execute this block if I directly run this file."
+    print("Base URL:", settings.base_url)
+    print("API Base URL:", settings.api_base_url)
+    print("Admin Email:", settings.admin_email)
+    print("Admin Password Configured:", bool(settings.admin_password))
